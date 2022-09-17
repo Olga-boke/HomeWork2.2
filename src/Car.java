8d9e706129ab18be3146ae85d07af11fdb18eeb3
Olga <dochnohi@yandex.ru>
2022-09-17 16:21:34 +0300

    Готовое дз

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+public class Car extends Transport {
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку автомобиля");
+    }
+
+    @Override
+    public void transportService() {
+        updateTyre();
+        checkEngine();
+
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель автомобиля");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
+            Car car = new Car("Лада",4);
+
+
+            Truck truck = new Truck( "Камаз",8);
+
+
+            Bicycle bicycle = new Bicycle("Кама", 2);
+
+
+            ServiceStation station = new ServiceStation();
+            station.check(car);
+            System.out.println();
+            station.check(bicycle);
+            System.out.println();
+            station.check(truck);
     }
 }
